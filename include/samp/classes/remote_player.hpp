72634e2bf3c4abd1c411975a5615a4b466d23315
waparabka <68443529+waparabka@users.noreply.github.com>
2025-11-23 15:56:32 +0600

    Update remote_player.hpp
    fix #2

--- include/samp/classes/remote_player.hpp
+++ include/samp/classes/remote_player.hpp
@@ -167,7 +167,7 @@ namespace samp::classes {
         }
 
         CPed* get_ped() {
-            return ((CPed*)((uintptr_t)this + OFFSET(offsets::remote_player::get_ped)));
+            return *(CPed**)((uintptr_t)this + OFFSET(offsets::remote_player::get_ped));
         }
 
         float get_health() {
